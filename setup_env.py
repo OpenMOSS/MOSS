a2a0f4a13f1e3eb6e30a5d7a48eea89a3538ba5b
PanQiWei <594557445@qq.com>
2023-04-27 11:05:38 +0800

    make installation of cuda extension and triton optional

--- setup_env.py
+++ setup_env.py
@@ -26,6 +26,8 @@ def setup_env():
     parser.add_argument("--conda_name", type=str, default="moss")
     parser.add_argument("--python_version", type=str, default="3.10")
     parser.add_argument("--reinstall_torch", action="store_true")
+    parser.add_argument("--no_cuda_ext_for_auto_gptq", action="store_true")
+    parser.add_argument("--use_triton", action="store_true")
     args = parser.parse_args()
 
     if args.init_conda:
@@ -70,9 +72,14 @@ def setup_env():
         )
 
     for pip_dependency in pip_dependencies:
+        command = f"./conda run -n {args.conda_name} pip install {pip_dependency}"
+        if "auto-gptq" in pip_dependency and args.no_cuda_ext_for_auto_gptq:
+            command = "BUILD_CUDA_EXT=0 " + command
+        if "auto-gptq" in pip_dependency and args.use_triton:
+            command = command.replace("auto-gptq", "auto-gptq[triton]")
         print(
             subprocess.run(
-                f"./conda run -n {args.conda_name} pip install {pip_dependency}".split(),
+                command.split(),
                 check=True,
                 stdout=subprocess.PIPE,
                 cwd=args.conda_home
