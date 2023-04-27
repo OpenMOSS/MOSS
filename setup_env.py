04562ca452ab250e4a6c0bda5d4eae4223da6b07
PanQiWei <594557445@qq.com>
2023-04-27 11:29:23 +0800

    make installation of auto_gptq optional

--- setup_env.py
+++ setup_env.py
@@ -6,6 +6,8 @@ from argparse import ArgumentParser
 # change version based on your situation
 pip_torch = "torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116"
 
+pip_auto_gptq = "auto-gptq -i https://pypi.org/simple"
+
 pip_dependencies = [
     "transformers==4.25.1",
     "sentencepiece",
@@ -14,9 +16,19 @@ pip_dependencies = [
     "matplotlib",
     "huggingface_hub",
     "gradio",
-    "auto-gptq -i https://pypi.org/simple",
     "mdtex2html"
 ]
+
+
+def run_command_and_show(cmd: str, conda_home):
+    print(
+        subprocess.run(
+            cmd.split(),
+            check=True,
+            stdout=subprocess.PIPE,
+            cwd=conda_home
+        ).stdout.decode()
+    )
 
 
 def setup_env():
@@ -40,55 +52,50 @@ def setup_env():
         help="python version used when creating conda env"
     )
     parser.add_argument("--reinstall_torch", action="store_true", help="whether to reinstall pytorch or not.")
+    parser.add_argument("--install_auto_gptq", action="store_true", help="whether to install auto-gptq")
     parser.add_argument(
         "--no_cuda_ext_for_auto_gptq",
         action="store_true",
-        help="whether to not install CUDA extension for auto-gptq"
+        help="whether to not install CUDA extension for auto-gptq, only effects when set flag --install_auto_gptq"
     )
-    parser.add_argument("--install_triton", action="store_true", help="whether to install triton")
+    parser.add_argument(
+        "--install_triton",
+        action="store_true",
+        help="whether to install triton, only effects when set flag --install_auto_gptq"
+    )
     args = parser.parse_args()
 
     if args.init_conda:
-        print(
-            subprocess.run(
-                f"./conda create -n {args.conda_name} python={args.python_version} -y".split(),
-                check=True,
-                stdout=subprocess.PIPE,
-                cwd=args.conda_home
-            ).stdout.decode()
+        run_command_and_show(
+            cmd=f"./conda create -n {args.conda_name} python={args.python_version} -y",
+            conda_home=args.conda_home
         )
 
     if args.reinstall_torch:
-        print(
-            subprocess.run(
-                f"./conda run -n {args.conda_name} pip uninstall torch -y".split(),
-                check=True,
-                stdout=subprocess.PIPE,
-                cwd=args.conda_home
-            ).stdout.decode()
+        run_command_and_show(
+            cmd=f"./conda run -n {args.conda_name} pip uninstall torch -y",
+            conda_home=args.conda_home
         )
-        print(
-            subprocess.run(
-                f"./conda run -n {args.conda_name} pip install {pip_torch}".split(),
-                check=True,
-                stdout=subprocess.PIPE,
-                cwd=args.conda_home
-            ).stdout.decode()
+        run_command_and_show(
+            cmd=f"./conda run -n {args.conda_name} pip install {pip_torch}",
+            conda_home=args.conda_home
         )
 
     for pip_dependency in pip_dependencies:
-        command = f"./conda run -n {args.conda_name} pip install {pip_dependency}"
-        if "auto-gptq" in pip_dependency and args.no_cuda_ext_for_auto_gptq:
-            command = f"./conda run -n {args.conda_name} BUILD_CUDA_EXT=0 pip install {pip_dependency}"
-        if "auto-gptq" in pip_dependency and args.install_triton:
+        run_command_and_show(
+            cmd=f"./conda run -n {args.conda_name} pip install {pip_dependency}",
+            conda_home=args.conda_home
+        )
+
+    if args.install_auto_gptq:
+        command = f"./conda run -n {args.conda_name} pip install {pip_auto_gptq}"
+        if args.no_cuda_ext_for_auto_gptq:
+            command = f"./conda run -n {args.conda_name} BUILD_CUDA_EXT=0 pip install {pip_auto_gptq}"
+        if args.install_triton:
             command = command.replace("auto-gptq", "auto-gptq[triton]")
-        print(
-            subprocess.run(
-                command.split(),
-                check=True,
-                stdout=subprocess.PIPE,
-                cwd=args.conda_home
-            ).stdout.decode()
+        run_command_and_show(
+            cmd=command,
+            conda_home=args.conda_home
         )
 
 
