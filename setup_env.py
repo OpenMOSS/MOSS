6c550329aeed6f6692a10b8e199280bf72264d40
PanQiWei <594557445@qq.com>
2023-04-27 11:11:26 +0800

    bug fix

--- setup_env.py
+++ setup_env.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 """WARNING: this scripts may only works on linux"""
 
 # change version based on your situation
-pip_torch = "torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116",
+pip_torch = "torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116"
 
 pip_dependencies = [
     "transformers==4.25.1",
